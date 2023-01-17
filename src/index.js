5dcd26c027acc0df5832192c6229da8041e7b4a9
Hichxm <hicham.slimani@digityourdream.fr>
2023-01-17 12:58:02 +0100

    feat: change details

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('widget-cmonbien'));
 
 root.render(
   <React.StrictMode>
