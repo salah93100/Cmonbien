5264b4138de206ba8f2f07ad72bda6e26ec1689f
Hichxm <hicham.slimani@digityourdream.fr>
2023-02-10 20:09:29 +0100

    fix

--- src/components/bookingComponents/BookingUI.js
+++ src/components/bookingComponents/BookingUI.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function BookingUI({ children }) {
   return (
-    <div className="max-h-screen w-full ">
+    <div className="w-full ">
       <section className="">
-        <div className="flex justify-center	">
+        <div className="flex justify-center">
           <div className="max-w-md w-full my-8 py-8 mx-4">{children}</div>
         </div>
       </section>
